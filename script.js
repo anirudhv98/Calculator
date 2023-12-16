fd98cc421c4486e7c3ef401422cd32bb92576594
anirudhv98 <vanirudh98@gmail.com>
2023-12-16 12:00:07 +0530

    Calc functionalities created

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-function addition(a,b)
-{
-    console.log(a+b);
+function addition(a, b) {
+  console.log(a + b);
 }
 
-function subtraction(a,b)
-{
-    console.log(a-b);
+function subtraction(a, b) {
+  console.log(a - b);
 }
 
-function multiplication(a,b)
-{
-    console.log(a*b);
+function multiplication(a, b) {
+  console.log(a * b);
 }
 
-function division(a,b)
-{
-    console.log(a / b);
+function division(a, b) {
+  console.log(a / b);
 }
+
+function calc(a, b, operator) {
+  if (operator === "+") {
+    addition(a, b);
+  } else if (operator === "-") {
+    subtraction(a, b);
+  } else if (operator === "*") {
+    multiplication(a, b);
+  } else if (operator === "/") {
+    division(a, b);
+  }
+}
+
+var a = prompt("Enter first number");
+var b = prompt("Enter second number");
+var operator = prompt("Enter operator");
+
+a = Number(a);
+b = Number(b);
+
+calc(a,b,operator);
