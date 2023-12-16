8d64814f7974e35251131cde883c732005a1fad0
anirudhv98 <vanirudh98@gmail.com>
2023-12-16 11:22:37 +0530

    Created Boilerplate and defined functions for basic operations

---
+++ script.js
@@ -0,0 +1,19 @@
+function addition(a,b)
+{
+    console.log(a+b);
+}
+
+function subtraction(a,b)
+{
+    console.log(a-b);
+}
+
+function multiplication(a,b)
+{
+    console.log(a*b);
+}
+
+function division(a,b)
+{
+    console.log(a / b);
+}
